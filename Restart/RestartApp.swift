d4c4ff2ca0d7fa0a72501cd256a5d00f87b5d764
David Viloria Ortega <david.viloria0424@gmail.com>
2024-08-24 23:04:59 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by David Viloria Ortega on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
