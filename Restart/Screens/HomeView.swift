9afab387d22cd3a256efdc285b2af3e4101a7ec4
David Viloria Ortega <david.viloria0424@gmail.com>
2024-09-19 22:42:51 -0500

    Final touches to onboarding view

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -12,13 +12,32 @@ struct HomeView: View {
 
     var body: some View {
         VStack(spacing: 30) {
-            Text("Home")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundColor(Color.red)
-            Button("Restart") {
+            // MARK: - Header
+            Spacer()
+            Image("character-2")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            // MARK: - Center
+            Text("The time that leads to mastery is dependent on the internsity of our focus")
+                .font(.system(.title3, design: .rounded, weight: .light))
+                .multilineTextAlignment(.center)
+                .padding()
+                .foregroundStyle(.secondary)
+            // MARK: - Footer
+            Spacer()
+            Button {
                 isOnboardingActive = true
+            } label: {
+                Image(systemName: "arrow.left.arrow.right.circle.fill")
+                    .imageScale(.large)
+                Text("Restart")
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            
+
         }
     }
 }
--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -9,22 +9,90 @@ import SwiftUI
 
 struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingActive : Bool = true
-
     
     var body: some View {
-        VStack {
-            Text("Onboarding")
-                .font(.title)
-                .fontWeight(.heavy)
-                .foregroundColor(Color("AccentColor"))
-                .multilineTextAlignment(.center)
-            
-            Button(action: {
-                isOnboardingActive = false
-            }, label: {
-                Text("Start")
-            })
-        }
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea()
+            VStack(spacing: 20) {
+                // MARK: - Header
+                Spacer()
+                VStack(spacing: 0, content: {
+                    Text("Share")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .fontWidth(.standard)
+                        .foregroundStyle(.white)
+                    
+                    Text("""
+                    It's not how much we give but how much love we put into giving
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundStyle(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                })
+                // MARK: - Center
+                ZStack(content: {
+                    Circle()
+                        .stroke(.white.opacity(0.2), lineWidth: 40)
+                        .frame(width: 260, height: 260, alignment: .center)
+                    Circle()
+                        .stroke(.white.opacity(0.2), lineWidth: 80)
+                        .frame(width: 260, height: 260, alignment: .center)
+                    Image("character-3")
+                        .resizable()
+                        .scaledToFit()
+                })
+                Spacer()
+                // MARK: - Footer
+                ZStack(content: {
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    Text("Get Started")
+                        .font(.system(size: 18, weight: .bold, design: .rounded))
+                        .foregroundStyle(.white)
+                        .offset(x: 15)
+                    
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        ZStack{
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(Color.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight:
+                                        .heavy, design: .rounded))
+                            
+                        }
+                        .foregroundStyle(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingActive = false
+                        }
+                        Spacer()
+                        
+                    }
+                    
+                })
+                .frame(height: 80, alignment: .center)
+                .padding()//: Footer
+            }
+        }//: Zstack
     }
 }
 
